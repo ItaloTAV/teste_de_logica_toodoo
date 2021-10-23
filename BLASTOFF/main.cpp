60aa9c4b3db901d1ec6bea41c280dcabf3180bca
ItaloTAV <italotavarespereira@hotmail.com>
2021-10-22 22:05:12 -0300

    Terceira Implementação

--- BLASTOFF/main.cpp
+++ BLASTOFF/main.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int problema1();
 int problema2();
+int problema3();
+int problema4();
 
 int main() {
 	//Acentuação
 	setlocale(LC_ALL,"");
 	problema1();
 	problema2();
+	problema3();
+	problema4();
 	
 	
 	return 0;
@@ -37,7 +41,40 @@ int problema2(){
 	cin >> comb;
 	
 	float b = a/comb;
-	cout << "O consumo médio é de " << b << " km/l.";	
+	cout << "O consumo médio é de " << b << " km/l.\n";	
 }
+
+int problema3(){
+	//3 - Dados três números (a, b, c), faça um algoritmo para mostrar o menor número.
+	int a,b,c;
+	cout << "\nDigite três números: \n";
+	cin >> a >> b >> c;
+	
+	if (a < b){
+		if (a < c){
+		cout << a << " é o menor numero!\n";
+		}
+		else
+		cout << c << " é o menor numero!\n";
+	}
+	else
+		if (b < c){
+			cout << b << " é o menor numero!\n";
+			}
+			else
+			cout << c << " é o menor numero!\n";
+		}
+		
+int problema4(){
+	//4 - Faça um algoritmo que converta a temperatura de C para F.
+	float celsius, fahr;
+	cout << "\n Digite a temperatura a ser convertida: ";
+	cin >> celsius;
+	fahr = (celsius * 9/5) + 32 ;
+	
+	cout << "\nA temperatura é " <<fahr << " Fahrenheit!";
+}
+
+
 
 
